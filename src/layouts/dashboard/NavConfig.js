adfa231e441038b98dc611cf3c41b22383aeecfb
Tharindu7Kanchana <7tharindugalle@gmail.com>
2022-09-06 04:48:51 +0530

    Update NavConfig.js

--- src/layouts/dashboard/NavConfig.js
+++ src/layouts/dashboard/NavConfig.js
@@ -114,6 +114,31 @@ const ownerConfig = [
 
 const bidderConfig = [
   {
+    title: "Overview",
+    path: "/dashboard/app",
+    icon: getIcon("eva:pie-chart-2-fill"),
+  },
+  {
+    title: "Users",
+    path: "/dashboard/user",
+    icon: getIcon("eva:people-fill"),
+  },
+  {
+    title: "Auction",
+    path: "/dashboard/auction",
+    icon: getIcon("eva:shopping-bag-fill"),
+  },
+  {
+    title: "Notices",
+    path: "/dashboard/notices",
+    icon: getIcon("eva:file-text-fill"),
+  },
+  {
+    title: "Profile",
+    path: "/dashboard/owner/profile",
+    icon: getIcon("gg:profile"),
+  },
+  {
     title: "login",
     path: "/login",
     icon: getIcon("eva:lock-fill"),
